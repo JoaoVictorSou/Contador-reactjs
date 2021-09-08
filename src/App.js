c54be9ba707624488c28ed8f35fa0f1f3c5bd7d6
João Victor <soulima.joao@gmail.com>
2021-09-08 11:41:09 -0300

    incremento e decremento

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
 
+import Contador from './components/Contador';
 function App() {
   return (
     <div className="App">
-     
+      <Contador numeroInicial={100}/>
     </div>
   );
 }
